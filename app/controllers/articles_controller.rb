b5ee3a7c85f2f6938f0eac7abf987c4b34fe66c9
Aron yu <h0935674938@hotmail.com>
2016-02-05 14:54:35 +0800

    add create article function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,35 @@
 class ArticlesController < ApplicationController
+	before_action :authenticate_user!, :only => [:create,:new]
 
 	def index
 	end
 	
-	def create
+	def new
+		@article = Article.new
 	end	
+
+	def create
+		@article = current_user.articles.new(article_params)
+
+		if @article.save
+			redirect_to articles_path
+			flash[:notice] = "建立成功"
+		else
+		  render :action => :new	
+		end  
+	end	
+
+
+
+
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) << :name
+      devise_parameter_sanitizer.for(:account_update) << :name
+    end
+
+    def article_params
+    	params.require(:article).permit(:title,:content,:category_id)
+    end	
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160205041950) do
+ActiveRecord::Schema.define(version: 20160205050451) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -28,5 +28,27 @@ ActiveRecord::Schema.define(version: 20160205041950) do
   add_index "articles", ["category_id"], name: "index_articles_on_category_id"
   add_index "articles", ["user_id"], name: "index_articles_on_user_id"
   add_index "articles", ["viewer"], name: "index_articles_on_viewer"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "fb_uid"
+    t.string   "fb_token"
+    t.string   "role"
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
